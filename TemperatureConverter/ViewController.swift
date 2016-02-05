826677f0f1d909181589d3902e72c82c09fec818
miguel a hernandez <voltaire33010@gmail.com>
2016-02-04 22:44:37 -0500

    commit 1

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -10,6 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var switchTemp: UISegmentedControl!
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var temperatureTextField: UITextField!
+    
+    @IBOutlet var temImage: UIImageView!
+    
+    @IBAction func converterButtom(sender: AnyObject) {
+        
+      print("El boton esta trabajando")
+        if (switchTemp.selectedSegmentIndex==0){
+        print("you got celsium")
+            let tmpe=Int(temperatureTextField.text!)!*9/5+32
+            
+            resultLabel.text="\(tmpe) farenheit°"
+            if(tmpe < 12){
+           
+            temImage.image=UIImage(named:"tempe.jpg")
+
+            }
+            
+            else{
+             temImage.image=UIImage(named:"temhot.jpg")
+            }
+        }
+        else{
+        print("yu got farenheit")
+            let tempf=(Int(temperatureTextField.text!)!-32)*5/9
+              resultLabel.text="\(tempf) celsium°"
+            temImage.image=UIImage(named:"tempe.jpg")
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
